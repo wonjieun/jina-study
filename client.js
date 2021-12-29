b2a62ee0d359694a28e58c5aa14b4ef754f2f5d7
Jina <wonjieunkk@gmail.com>
2021-12-29 13:56:04 +0900

    📦 NEW: client.js client rendering

---
+++ client.js
@@ -0,0 +1,20 @@
+import 'babel-core/register';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+function run() {
+  // to hydrate a container whose HTML contents were rendered by ReactDOMServer(in server.js)
+  ReactDOM.hydrate(<App />, document.getElementById('app'));
+}
+
+const loadedStates = ['complete', 'loaded', 'interactive'];
+
+if (loadedStates.includes(document.readyState) && document.body) { // when completely loaded HTML page in browser
+  run();
+} else {
+  window.addEventListener('DOMContentLoaded', event => {
+    run();
+    console.log('DOM fully loaded and parsed');
+  }, false);
+}
