75530a9097792bd38549592c8cbc365b465a71fa
Jina <wonjieunkk@gmail.com>
2022-03-04 21:21:32 +0900

    중간 데이터로 옮기기

--- 03/statement.js
+++ 03/statement.js
@@ -2,13 +2,15 @@ import invoices from "./invoices.json";
 import plays from "./plays.json";
 
 function statement(invoice, plays) {
-  const statement = {};
-  return renderPlainText(statement, invoice, plays);
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 }
 
-function renderPlainText(data, invoice, plays) {
-  let result = `\n청구 내역 (고객명: ${invoice.customer})\n`;
-  for (let perf of invoice.performances) {
+function renderPlainText(data, plays) {
+  let result = `\n청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
     result +=
       `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
@@ -18,7 +20,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalAmount() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += amountFor(perf);
     }
     return result;
@@ -26,7 +28,7 @@ function renderPlainText(data, invoice, plays) {
 
   function totalVolumeCredits() {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
