61b355787a2c0282a1abdef5855f369426b922ae
Jina <wonjieunkk@gmail.com>
2022-03-04 22:48:03 +0900

    HTML 버전 작성

--- 03/statement.js
+++ 03/statement.js
@@ -2,6 +2,32 @@ import invoices from "./invoices.json";
 import plays from "./plays.json";
 import createStatementData from "./createStatementData.js";
 
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+  for (let perf of data.performances) {
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)}</td></tr>`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: ${usd(data.totalAmount)}\n</p>`;
+  result += `<p>적립 포인트: ${data.totalVolumeCredits}점\n</p>`;
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
+
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
@@ -14,14 +40,6 @@ function renderPlainText(data) {
   result += `총액: ${usd(data.totalAmount)}\n`;
   result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 }
 
 console.log(statement(invoices, plays));
