db42531425e5e3bf5f851ba5228ac4f8e13533c3
Jina <wonjieunkk@gmail.com>
2022-03-04 22:18:02 +0900

    중간 데이터로 옮기기5

--- 03/statement.js
+++ 03/statement.js
@@ -5,6 +5,8 @@ function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -48,17 +50,6 @@ function statement(invoice, plays) {
       result += Math.floor(aPerformance.audience / 5);
     return result;
   }
-}
-
-function renderPlainText(data, plays) {
-  let result = `\n청구 내역 (고객명: ${data.customer})\n`;
-  for (let perf of data.performances) {
-    result +=
-      `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-  }
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
-  return result;
 
   function totalAmount() {
     let result = 0;
@@ -75,6 +66,17 @@ function renderPlainText(data, plays) {
     }
     return result;
   }
+}
+
+function renderPlainText(data, plays) {
+  let result = `\n청구 내역 (고객명: ${data.customer})\n`;
+  for (let perf of data.performances) {
+    result +=
+      `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+  result += `총액: ${usd(perf.totalAmount)}\n`;
+  result += `적립 포인트: ${perf.totalVolumeCredits}점\n`;
+  return result;
 
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
