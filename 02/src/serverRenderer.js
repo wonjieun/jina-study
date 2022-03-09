ee70206c406dcafeb51da1ac7cf1f9369ca93772
Jina <wonjieunkk@gmail.com>
2022-03-09 17:12:51 +0900

    set window.$state in serverRenderer

--- 02/src/serverRenderer.js
+++ 02/src/serverRenderer.js
@@ -1,9 +1,12 @@
-export const serverRenderer = (RootComponent) => `
+export const serverRenderer = (RootComponent, state) => `
   <!doctype html>
   <html lang="ko">
   <head>
     <meta charset="UTF-8">
     <title>Server Side Rendering</title>
+    <script>
+      window.$state = ${JSON.stringify(state)}
+    </script>
   </head>
   <body>
     <div id="app">${RootComponent}</div>
