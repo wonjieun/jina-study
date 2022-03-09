4f839ddcb24c4a5aed8989fc983c0c9b07f6e106
Jina <wonjieunkk@gmail.com>
2022-03-09 17:55:39 +0900

    📦 NEW: function hydration in store

--- 02/src/store/index.js
+++ 02/src/store/index.js
@@ -6,11 +6,15 @@ export const store = {
       { id: 1, userId: 'admin', password: 'admin1' },
     ],
   },
+  hydrationToServer(state) {
+    // using client-side JavaScript to add application state and interactivity to server-rendered HTML
+    this.state = state;
+  },
   setState (newState) {
-    this.state = { ...this.state, ...newState }
+    this.state = { ...this.state, ...newState };
   },
   addUserItem(userItem) {
-    const newUserItem = { id: this.state.userList.length + 1, ...userItem }
+    const newUserItem = { id: this.state.userList.length + 1, ...userItem };
     this.setState({ userList: [...this.state.userList, newUserItem] });
   }
 };
