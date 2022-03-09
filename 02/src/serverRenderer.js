4a55f70977415ebcc5c4144b28f375b7ccc6f90d
Jina <wonjieunkk@gmail.com>
2022-03-09 17:58:41 +0900

    👌 IMPROVE: refactoring indent HTML

--- 02/src/serverRenderer.js
+++ 02/src/serverRenderer.js
@@ -1,16 +1,16 @@
 export const serverRenderer = (RootComponent, state) => `
   <!doctype html>
   <html lang="ko">
-  <head>
-    <meta charset="UTF-8">
-    <title>Server Side Rendering</title>
-    <script>
-      window.$state = ${JSON.stringify(state)}
-    </script>
-  </head>
-  <body>
-    <div id="app">${RootComponent}</div>
-    <script src="./src/main.js" type="module"></script>
-  </body>
+    <head>
+      <meta charset="UTF-8">
+      <title>Server Side Rendering</title>
+      <script>
+        window.state = ${JSON.stringify(state)}
+      </script>
+    </head>
+    <body>
+      <div id="app">${RootComponent}</div>
+      <script src="./src/main.js" type="module"></script>
+    </body>
   </html>
 `;
