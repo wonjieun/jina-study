f3d0f374fabfc4751cea04c5b1ff10a100cce2cc
Jina <wonjieunkk@gmail.com>
2022-05-21 15:19:53 +0900

    여러 함수를 클래스로 묶기(Combine Functions into Class)

---
+++ 03 | Refactoring/10/Bird.js
@@ -0,0 +1,31 @@
+export default class Bird {
+  constructor(birdObject) {
+    Object.assign(this, birdObject);
+  }
+
+  get plumage() {
+    switch (this.type) {
+      case '유럽 제비':
+        return '보통이다';
+      case '아프리카 제비':
+        return this.numberOfCoconuts > 2 ? '지쳤다' : '보통이다';
+      case '노르웨이 파랑 앵무':
+        return this.voltage > 100 ? '그을렸다' : '예쁘다';
+      default:
+        return '알 수 없다';
+    }
+  }
+
+  get airSpeedVelocity() {
+    switch (this.type) {
+      case '유럽 제비':
+        return 35;
+      case '아프리카 제비':
+        return 40 - 2 * this.numberOfCoconuts;
+      case '노르웨이 파랑 앵무':
+        return this.isNailed ? 0 : 10 + this.voltage / 10;
+      default:
+        return null;
+    }
+  }
+}
--- 03 | Refactoring/10/index.js
+++ 03 | Refactoring/10/index.js
@@ -1,30 +1,11 @@
-function plumages(birds) {
-	return new Map(birds.map(b => [b.name, plumage(b)]));
+import Bird from './Bird.js';
+
+function plumages(bird) {
+  return new Bird(bird).plumage;
 }
-function speeds(birds) {
-	return new Map(birds.map(b => [b.name, airSpeedVelocity(b)]));
+function speeds(bird) {
+  return new Bird(bird).airSpeedVelocity;
 }
-function plumage(bird) {
-	switch (bird.type) {
-		case '유럽 제비':
-			return '보통이다';
-		case '아프리카 제비';
-			return (bird.numberOfCoconuts > 2) ? '지쳤다' : '보통이다';
-		case '노르웨이 파랑 앵무';
-			return (bird.voltage > 100) ? '그을렸다' : '예쁘다';
-		default:
-			return '알 수 없다';
-	}
-}
-function airSpeedVelocity(bird) {
-  switch (bird.type) {
-    case '유럽 제비':
-      return 35;
-    case '아프리카 제비':
-      return 40 - 2 * bird.numberOfCoconuts;
-    case '노르웨이 파랑 앵무':
-      return (bird.isNailed) ? 0 : 10 + bird.voltage / 10;
-    default:
-      return null;
-  }
-}
+
+console.log(plumages({ type: '유럽 제비' }));
+console.log(speeds({ type: '유럽 제비' }));
